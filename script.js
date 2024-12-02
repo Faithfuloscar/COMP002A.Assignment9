98535e660ea289b86df477e436c41e30b50544c7
Faithfuloscar <oscarmendez2003@outlook.com>
2024-12-01 23:27:56 -0800

    completed

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("The use of online resources such as youtube, w3schools, and stackoverflow have been of great aid.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("The length, Javascript is a language that should be given more time to learn in order to properly understand concepts");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I had no idea what to expect, but overall I learned concepts that will serve in my future endeavors in CompSci.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,6 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log(
+  "The concepts that were new to me had to deal with arrays and different ways of using them. Previously I had already used functions in the C# course."
+);
